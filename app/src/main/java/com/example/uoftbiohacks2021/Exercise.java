97e67088bd750189e83258b725db80013c120b81
zhenw01 <zhenw01@gmail.com>
2021-03-13 16:35:23 -0500

    attempt at exercise spinner

--- app/src/main/java/com/example/uoftbiohacks2021/Exercise.java
+++ app/src/main/java/com/example/uoftbiohacks2021/Exercise.java
@@ -4,6 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.widget.AdapterView;
+
+import android.widget.ArrayAdapter;
+
+import android.widget.Spinner;
+
+import android.widget.Toast;
+
 
 public class Exercise extends AppCompatActivity {
 
@@ -12,26 +22,13 @@ public class Exercise extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
         //this is a yikes please change it
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("JAVA");
-        arrayList.add("ANDROID");
-        arrayList.add("C Language");
-        arrayList.add("CPP Language");
-        arrayList.add("Go Language");
-        arrayList.add("AVN SYSTEMS");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,                         android.R.layout.simple_spinner_item, arrayList);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                string tutorialsName = parent.getItemAtPosition(position).toString();
-                Toast.makeText(parent.getContext(), "Selected: " + tutorialsName,          Toast.LENGTH_LONG).show();
-            }
-            @Override
-            public void onNothingSelected(AdapterView <?> parent) {
-            }
-        });
+        String[] arraySpinner = new String[] {
+                "Extremely Sedentary", "Sedentary", "Average", "Active", "Extremely Active"
+        };
+        Spinner s = (Spinner) findViewById(R.id.Exercisespinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
 }
