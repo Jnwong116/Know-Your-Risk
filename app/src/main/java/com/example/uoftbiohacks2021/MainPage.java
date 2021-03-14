04d071684f90bc7d6e16a80cdd102a042d486a54
Jordan <jnwong116@gmail.com>
2021-03-13 20:54:37 -0500

    Added registration code

--- app/src/main/java/com/example/uoftbiohacks2021/MainPage.java
+++ app/src/main/java/com/example/uoftbiohacks2021/MainPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.uoftbiohacks2021.logic.User;
-
-import org.w3c.dom.Text;
+import com.example.uoftbiohacks2021.logic.UserWriter;
 
 public class MainPage extends AppCompatActivity {
     private User currentUser;
@@ -28,4 +28,15 @@ public class MainPage extends AppCompatActivity {
         this.title = findViewById(R.id.WelcomeMessage);
         this.title.setText("Hello " + currentUser.getUsername() + "! Here are some recommended activities. Be sure to let us know if there are any changes to your health status.");
     }
+
+    public void moreInfo(View view) {
+        Intent intent = new Intent(this, additional_information.class);
+        intent.putExtra("currentUser", currentUser);
+        startActivity(intent);
+    }
+
+    /*public void updateHealth(View view) {
+        Intent intent = new Intent(this, );
+        startActivity(intent);
+    }*/
 }
--- app/src/main/java/com/example/uoftbiohacks2021/additional_information.java
+++ app/src/main/java/com/example/uoftbiohacks2021/additional_information.java
@@ -2,13 +2,33 @@ package com.example.uoftbiohacks2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.uoftbiohacks2021.logic.User;
+import com.example.uoftbiohacks2021.logic.UserWriter;
+
 public class additional_information extends AppCompatActivity {
+
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additional_information);
+
+        Intent intent = getIntent();
+        currentUser = (User) intent.getSerializableExtra("currentUser");
+        currentUser.setContext(getApplicationContext());
+
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        Intent back = new Intent(this, MainPage.class);
+        back.putExtra("currentUser", currentUser);
+        startActivity(back);
+        return true;
     }
 }
