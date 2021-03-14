6cd20d423f6b5a22659ffc37ae3d12833669074b
zhenw01 <zhenw01@gmail.com>
2021-03-13 19:30:43 -0500

    updated risk factor questions

--- app/src/main/java/com/example/uoftbiohacks2021/RiskFactorQuestions.java
+++ app/src/main/java/com/example/uoftbiohacks2021/RiskFactorQuestions.java
@@ -3,6 +3,8 @@ package com.example.uoftbiohacks2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.ArrayAdapter;
 
 public class RiskFactorQuestions extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class RiskFactorQuestions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_risk_factor_questions);
+        AutoCompleteTextView autocomplete;
+
+        String[] arr = { "Cancer", "Chronic kidney disease", "Chronic obstructive pulmonary disease", "Down syndrome", "Heart conditions", "Immunocompromised (organ transplant)", "Obesity (BMI from 30 to 40)",
+                "Severe Obesity (BMI over 40)", "Pregnancy", "Sickle cell anemia", "Smoking", "Type 2 diabetes (pancreatic)", /* These all have risk values of 15 */
+                "Asthma", "Cerebrovascular disease", "Cystic fibrosis", "High blood pressure (Hypertension)", "Immunocompromised (blood or marrow transplant)", "Neurological condition (e.g. dementia)",
+                "Liver disease", "Overweight (BMI between 25 and 30)", "Pulmonary fibrosis", "Thalassemia", "Type 1 diabetes (pancreatic)" /* These all have risk values of 10*/
+        }
+
+
+            autocomplete = (AutoCompleteTextView)
+                    findViewById(R.id.autoCompleteTextView1);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                    (this,android.R.layout.select_dialog_item, arr);
+
+            autocomplete.setThreshold(2);
+            autocomplete.setAdapter(adapter);
+        }
     }
-}
